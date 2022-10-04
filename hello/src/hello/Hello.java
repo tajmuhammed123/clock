3f81872536f6088a090b8f62fdfbb06508667566
Taj Muhammed <44499013+tajmuhammed123@users.noreply.github.com>
2022-10-04 12:56:12 +0530

    Delete hello directory

--- hello/src/hello/Hello.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-public class Hello {
-	public static void main(String[] args) {
-		None n=new None(10,20);
-	}
-}
--- hello/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author tajmu
- *
- */
-module hello {
-}
